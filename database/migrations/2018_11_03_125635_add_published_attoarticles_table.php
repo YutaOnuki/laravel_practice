8834128a31fa81a5c31fbe0601a6076cdaca1bd7
小貫祐汰 <onukiyuuta@onukiyuunoMacBook-ea.local>
2018-11-03 22:54:16 +0900

    Laravel 11 OK

--- database/migrations/2018_11_03_125635_add_published_attoarticles_table.php
+++ database/migrations/2018_11_03_125635_add_published_attoarticles_table.php
@@ -14,7 +14,7 @@ class AddPublishedAttoarticlesTable extends Migration
     public function up()
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->timestamp('published_at')->nullable();
+            $table->datetime('published_at')->nullable();
         });
     }
 
