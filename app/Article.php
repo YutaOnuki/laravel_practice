ee308cd4bc2b86c19c28ad8918188ac91ed924e7
小貫祐汰 <onukiyuuta@onukiyuunoMacBook-ea.local>
2018-11-04 14:04:08 +0900

    Laravel 12 OK

--- app/Article.php
+++ app/Article.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $guarded = ['id'];
+    protected $dates = ['published_at'];
+
+    //大文字変換アクセッサ
+    public function getTitleAttribute($value)
+    {
+      return mb_strtoupper($value);
+    }
+
+    //小文字変換ミューテータ
+    public function setTitleAttribute($value)
+    {
+      $this->attributes['title'] = mb_strtolower($value);
+    }
 }
