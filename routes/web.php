a6637e6c96d05a945fb2670a24c5d8cc6baee3da
小貫　祐汰 <onu4648yu@icloud.com>
2018-11-06 22:25:46 +0900

    Laravel 21 routes controller finished

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,5 @@ Route::get('articles', 'ArticlesController@index');
 Route::get('articles/create', 'ArticlesController@create');
 Route::get('articles/{id}', 'ArticlesController@show');
 route::post('articles', 'ArticlesController@store');
+Route::get('articles/{id}/edit', 'ArticlesController@edit');
+Routepatch('articles/{id}', 'ArticlesController@update');
